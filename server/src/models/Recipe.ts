d00312c1576e4a3fa7d54e323b530e9020086237
bungin <nimai.band@gmail.com>
2024-11-27 04:11:03 -0600

    progress

--- server/src/models/Recipe.ts
+++ server/src/models/Recipe.ts
@@ -1,5 +1,60 @@
-// need recipe props/args etc..
+import { Schema, model, Document } from "mongoose";
+import User from "./User.js";
 
+interface IComment extends Document {
+  commentAuthor: typeof User;
+  commentText: string;
+  createdAt: Date;
+}
 
+interface IRecipe extends Document {
+  recipeAuthor: string;
+  recipeName: string;
+  recipeDescription: string;
+  recipeIngredients: string[];
+  recipeInstructions: string[];
+  recipeComments: IComment[];
+  createdAt: Date;
+}
+
+const commentSchema = new Schema<IComment>(
+  {
+    commentText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 500,
+    },
+    commentAuthor: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    _id: false,
+    toJSON: { getters: true },
+    toObject: { getters: true },
+    timestamps: true,
+  }
+);
+
+const recipeSchema = new Schema<IRecipe>({
+  recipeDescription: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 500,
+    trim: true,
+  },
+  recipeIngredients: {
+    type: [String],
+    required: true,
+    validate: {
+      validator: (ingredients: string[]) => ingredients.length > 0,
+      message: "Must have at least one ingredient",
+    },
+  },
+});
 
 export default Recipe;
