880c06b58c718673485d929830341bf8152d41b6
bungin <nimai.band@gmail.com>
2024-12-05 19:37:53 -0600

    remove redundent

--- client/src/components/LikeButton/index2.tsx
+++
@@ -1,6 +0,0 @@
-import { useMutation } from "@apollo/client";
-import { useState } from "react";
-import { ADD_LIKE, REMOVE_LIKE } from "../../utils/mutations";
-import { useQuery } from "@apollo/client";
-import { QUERY_ME } from "../../utils/queries";
-
