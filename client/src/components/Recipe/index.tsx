e1df3b9e09b685e4e74d28e875e5c8eb232d1c8f
Laura Schooley <laura.schooley3@gmail.com>
2024-12-07 14:46:41 -0600

    adjusted settings so you must be logged in to see

--- client/src/components/Recipe/index.tsx
+++ client/src/components/Recipe/index.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"; 
+// import { Link } from "react-router-dom"; 
 import RecipeForm from "../RecipeForm/index";
 import RecipeList from "../RecipeList/index";
 import { Recipe } from "../../interfaces/Recipe";
 import "./index.css";
+
+import { useQuery } from "@apollo/client";
+import { QUERY_ME } from "../../utils/queries";
 
 interface RecipesProps {
   recipes: Recipe[];
@@ -11,6 +14,11 @@ interface RecipesProps {
 
 const Recipes: React.FC<RecipesProps> = ({ recipes }) => {
   const [showForm, setShowForm] = useState(false);  // State to control form visibility
+
+  const { loading, data } = useQuery(QUERY_ME);  // Query the logged-in user's data
+ // }
+
+  const user = data?.me || {};
 
   const handleAddRecipe = (newRecipe: Recipe) => {
     console.log(newRecipe);
@@ -20,24 +28,36 @@ const Recipes: React.FC<RecipesProps> = ({ recipes }) => {
     setShowForm((prev) => !prev);  // Toggle the form visibility when button is clicked
   };
 
+  if(loading) return <div>Loading...</div>;
+
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
+      </h4>
+    );
+  }
+
   return (
     <div>
-      <div className="header">
-        <h1>Create a Post</h1>
-        <button 
-          className="plus-button" 
-          aria-label="Create a Post" 
-          onClick={toggleForm}  // Toggle the visibility of RecipeForm
-        >
-          <span className="plus-icon">	
-          &#128993;</span>
-        </button>
-      </div>
-
-      {/* Conditionally render RecipeForm */}
-      {showForm && <RecipeForm onAddRecipe={handleAddRecipe} />}
-
-      <RecipeList recipes={recipes} title="All Recipes" />
+        <>
+          <div className="header">
+            <h1>Create a Post</h1>
+            <button 
+              className="plus-button" 
+              aria-label="Create a Post" 
+              onClick={toggleForm}  // Toggle the visibility of RecipeForm
+            >
+              <span className="plus-icon">	
+              &#128993;</span>
+            </button>
+          </div>
+    
+          {showForm && <RecipeForm onAddRecipe={handleAddRecipe} />}
+    
+          <RecipeList recipes={recipes} title="All Recipes" />
+        </>
     </div>
   );
 };
